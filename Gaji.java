f1966f435b1c20bf3660fb99c6ca255da6c327b2
adamrhayyapahlevi <adamrhayyapahlevi@gmail.com>
2024-12-07 23:52:21 +0700

    Tugas Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,4 +1,26 @@
- import java.util.Scanner;
+import java.util.Scanner;
+
+public class Gaji{
+    public static int masaKerja;
+
+    public static int gaji(int masaKerja){
+    if (masaKerja<5) {
+        return 5000000;
+    } else if (masaKerja>=5&&masaKerja<=10) {
+        return 7500000;
+    } else
+    return 10000000;
+    }
+
+    public static int lembur(int masaKerja){
+        if (masaKerja<5) {
+            return 40000;
+        } else if (masaKerja>=5&&masaKerja<=10) {
+            return 50000;
+        } else
+        return 75000;
+    }
+
 
 public class Gaji{
     public static int masaKerja;
@@ -9,6 +31,15 @@ public class Gaji{
         int jamLembur = input.nextInt();
 
         /* Pekerjaan anda mulai dari sini */
+
+        int gajiBulanan = gaji(masaKerja);
+        int lemburBulanan = lembur(masaKerja);
+
+        lemburBulanan*=jamLembur;
+        gajiBulanan+=lemburBulanan;
+
+        System.out.println(gajiBulanan);
+        
 
         /* Pekerjaan anda berakhir sini */
 
